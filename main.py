d555fb150c9e9abde5083c24ed105e54709e5191
AleksandrD <eeee@>
2025-01-28 17:21:57 +0500

    Commit Form PyCharm with tag2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from logs import init_log_file, write_log
 
 init_log_file2()
 write_log("[START]","Start mainqwfrefergerg.py")
-if IS_EXPORT:22222222222222
+if IS_EXPORT:333333333
     print('efewfrfrefergeg')
 write_log("[FINISH]","Finish main.py")
 print()
