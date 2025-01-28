8b8e1429fd5a27d15531b952331cbd8ece21d643
AleksandrD <AleksandrD@ntds255.zyfra.com>
2025-01-28 15:22:04 +0500

    changed main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from get_params import (IS_EXPORT)
 from export import export
 from logs import init_log_file, write_log
 
-init_log_file()
+init_log_file2()
 write_log("[START]","Start main.py")
 if IS_EXPORT:
-    export()
+    
 write_log("[FINISH]","Finish main.py")
+print()
