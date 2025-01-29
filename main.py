e41e630db51b3bce48dc0344c30b283741f57665
AleksandrD <eeee@>
2025-01-29 15:36:35 +0500

    qq

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from logs import init_log_file, write_log
 init_log_file2()
 write_log("[START]","Start mainqwfrefergerg.py")
 if IS_EXPORT:333333333
-    print('efewfrfrefergeg')
+    print('efewfrfreferg')
 write_log("[FINISH]","Finish main.py")
 print()
